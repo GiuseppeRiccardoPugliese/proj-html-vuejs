7c7d7371441e94abfdd36afefd3eeb5a50dae569
Davide Prencipe <davidedev04@gmail.com>
2024-01-16 13:49:52 +0100

    aggiunto fruits

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,6 +1,52 @@
 <script>
 export default {
-  name: 'About'
+  name: 'About',
+  data() {
+    return {
+      fruits: [
+        {
+          name: 'Natural Cabbage',
+          imgPath:
+            'https://themexbd.com/gogrin/organic/wp-content/uploads/2022/12/H3-fe1.png'
+        },
+        {
+          name: 'Fresh Tomato',
+          imgPath:
+            'https://themexbd.com/gogrin/organic/wp-content/uploads/2022/12/H3-fe2.png'
+        },
+        {
+          name: 'Organic Spinach',
+          imgPath:
+            'https://themexbd.com/gogrin/organic/wp-content/uploads/2022/12/H3-fe3.png'
+        },
+        {
+          name: 'Natural Carrots',
+          imgPath:
+            'https://themexbd.com/gogrin/organic/wp-content/uploads/2022/12/H3-fe4.png'
+        },
+        {
+          name: 'Organic Grapes',
+          imgPath:
+            'https://themexbd.com/gogrin/organic/wp-content/uploads/2022/12/H3-fe5.png'
+        },
+        {
+          name: 'Fresh Apple',
+          imgPath:
+            'https://themexbd.com/gogrin/organic/wp-content/uploads/2022/12/H3-fe6.png'
+        },
+        {
+          name: 'Natural Farming',
+          imgPath:
+            'https://themexbd.com/gogrin/organic/wp-content/uploads/2022/12/H2-fe4.png'
+        },
+        {
+          name: 'Organic Eggplant',
+          imgPath:
+            'https://themexbd.com/gogrin/organic/wp-content/uploads/2022/12/H2-fe1.png'
+        }
+      ]
+    }
+  }
 }
 </script>
 
@@ -35,7 +81,7 @@ export default {
             across the world.
           </p>
           <div class="d-flex">
-            <div class="w-parag">
+            <div class="w-parag me-2">
               <h3>All are handmade</h3>
               <p class="p_grey">
                 If I'm honest, the thing I am proudest of is my varieties of
@@ -61,7 +107,9 @@ export default {
               sustainable but not organic
             </p>
           </div>
-          <div class="my_button d-flex align-items-center justify-content-center p-3">
+          <div
+            class="my_button d-flex align-items-center justify-content-center p-3"
+          >
             <span><a href="#">ABOUT US</a></span>
           </div>
         </div>
@@ -74,15 +122,37 @@ export default {
       </div>
     </div>
 
-    <div class="container shop"></div>
+    <div class="my-5 container shop"></div>
+
+    <div class="d-flex justify-content-center align-items-center flex-column">
+      <div class="container row my-5">
+        <div class="col-12 col-md-4">
+          <h1>
+            Our organic <span class="s_orange">fruits</span> &
+            <span class="s_orange">vegetable</span>
+          </h1>
+          <p class="p_grey">
+            Organic is loaded with a sense of rightness, with a set of rules. I
+            would much rather someone bought food that was local and sustainable
+            but not organic than bought organic food
+          </p>
+        </div>
+        <div class="col-12 col-md-8 d-flex flex-wrap">
+          <div
+            class="card_fruits_org d-flex align-items-center flex-column mb-3"
+            v-for="item in fruits"
+          >
+            <img :src="item.imgPath" alt="" />
+            <span>{{ item.name }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
 <style lang="scss" scoped>
 @use '../styles/partials/variables' as *;
-img {
-  width: 100%;
-}
 
 .jumbo {
   height: 220px;
@@ -92,46 +162,50 @@ img {
 }
 
 .part_up {
-    h1{
-        font-weight: bold;
-    }
-    h3{
-        font-weight: bold;
-    }
-  .s_orange {
-    color: #ef9303;
+  img {
+    width: 100%;
   }
 
   .w-parag {
     width: 50%;
   }
 
-  .p_grey {
-    color: $bg-light_black;
-  }
-
-  .my_button{
+  .my_button {
     width: 140px;
     background-color: #ef9303;
     border-top-right-radius: 20px;
     border-bottom-left-radius: 20px;
     transition: background-color 0.5s linear 0s;
 
-    span{
-        font-size: 14px;
+    span {
+      font-size: 14px;
 
-        a{
-            text-decoration: none;
-            color: $primary;
-        }
+      a {
+        text-decoration: none;
+        color: $primary;
+      }
     }
-
   }
 
-  .my_button:hover{
+  .my_button:hover {
     background-color: $green;
     cursor: pointer;
   }
+}
+
+.s_orange {
+  color: #ef9303;
+}
+
+.p_grey {
+  color: $bg-light_black;
+}
+
+h1 {
+  font-weight: bold;
+}
+h3 {
+  font-weight: bold;
 }
 
 .shop {
@@ -139,5 +213,20 @@ img {
 
   //debug
   height: 500px;
+}
+
+.card_fruits_org {
+  width: 25%;
+
+  span {
+    font-weight: 500;
+    margin-top: 10px;
+  }
+}
+
+@media screen and (max-width: 768px) {
+    .card_fruits_org{
+        width: 50%;
+    }
 }
 </style>
