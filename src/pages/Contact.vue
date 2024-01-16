945504d30680150841583055882da0f1a43db654
Davide Prencipe <davidedev04@gmail.com>
2024-01-16 20:27:54 +0100

    breadcrump contact

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,15 +1,45 @@
 <script>
-
 export default {
-    name: 'Contact',
+  name: 'Contact'
 }
-
 </script>
 
 <template>
-    <h1>Contact</h1>
+  <main>
+    <div
+      class="jumbo d-flex justify-content-center align-items-center flex-column"
+    >
+      <div>
+        <h2>About</h2>
+      </div>
+      <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">You here!</li>
+          <li class="breadcrumb-item">> <a href="/">Home</a></li>
+          <li class="breadcrumb-item">> Contact</li>
+        </ol>
+      </nav>
+    </div>
+  </main>
 </template>
 
 <style lang="scss" scoped>
 @use '../styles/partials/variables' as *;
+
+.jumbo {
+  height: 220px;
+  background-image: url(../assets/breckum-img.jpg);
+  background-size: cover;
+  color: $primary;
+
+  a {
+    text-decoration: none;
+    color: $primary;
+    transition: color 0.3s linear 0s;
+  }
+
+  a:hover {
+    color: #ef9303;
+  }
+}
 </style>
