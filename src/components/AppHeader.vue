05b50c7799dbb82fb01cf8f09b3899e9309d1860
Davide Prencipe <davidedev04@gmail.com>
2024-01-16 12:59:44 +0100

    checkpoint

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -38,7 +38,7 @@ export default {
     </div>
   </header>
 
-  <!-- <nav>
+  <nav>
         <ul>
             <li>
                 <router-link :to="{name: 'home'}">Home</router-link>
@@ -50,7 +50,7 @@ export default {
                 <router-link :to="{name: 'contact'}">Contact</router-link>
             </li>
         </ul>
-    </nav> -->
+    </nav>
 </template>
 
 <style lang="scss" scoped>
