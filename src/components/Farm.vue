86a7ee81de12cb5572cbf014d616548e53c815b0
Davide Prencipe <davidedev04@gmail.com>
2024-01-17 15:50:43 +0100

    correzione

--- src/components/Farm.vue
+++ src/components/Farm.vue
@@ -67,13 +67,7 @@ export default {
       <img src="../assets/brand4.png" alt="" />
     </swiper-slide>
     <swiper-slide class="d-flex justify-content-center align-items-center">
-      <img src="../assets/brand4.png" alt="" />
-    </swiper-slide>
-    <swiper-slide class="d-flex justify-content-center align-items-center">
-      <img src="../assets/brand4.png" alt="" />
-    </swiper-slide>
-    <swiper-slide class="d-flex justify-content-center align-items-center">
-      <img src="../assets/brand4.png" alt="" />
+      <img src="../assets/brand1.png" alt="" />
     </swiper-slide>
   </swiper>
 </template>
