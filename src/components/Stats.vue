84e044557b6ff8ec721b9d1522d755107da6991b
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2024-01-18 11:19:40 +0100

    add: SetInterval

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,38 +1,83 @@
 <script>
 export default {
-    name: 'Stats',
-    
+  name: 'Stats',
+  data() {
+    return {
+      count: 0,
+      years: 0,
+      client: 0,
+      award: 0
+    }
+  },
+  created() {
+    this.startCount()
+  },
+  methods: {
+    startCount() {
+      const interval = setInterval(() => {
+        if (this.count < 200) {
+          this.count++;
+        } else {
+          clearInterval(interval);
+        }
+      }, 10);
+
+      const intervalYears = setInterval(() => {
+        if (this.years < 29) {
+          this.years++;
+        } else {
+          clearInterval(intervalYears);
+        }
+      }, 10);
+
+      const intervalClient = setInterval(() => {
+        if (this.client < 20) {
+          this.client++;
+        } else {
+          clearInterval(intervalClient);
+        }
+      }, 10);
+      const intervalAward = setInterval(() => {
+        if (this.award < 20) {
+          this.award++;
+        } else {
+          clearInterval(intervalAward);
+        }
+      }, 10);
+    }
+  }
+
 }
 </script>
 
 <template>
-    <div class="statistiche container d-flex justify-content-center align-items-center my-5">
-        <div class="d-flex justify-content-center align-items-center flex-column mx-4">
-          <h1 class="s_orange">29 +</h1>
-          <h5>Years of Experienced</h5>
-        </div>
-        <div>
-          <img src="../assets/counter-shap1.png" alt="">
-        </div>
-        <div class="d-flex justify-content-center align-items-center flex-column mx-4">
-          <h1 class="s_orange">200 +</h1>
-          <h5>Everyday Online Delivery</h5>
-        </div>
-        <div>
-          <img src="../assets/counter-shap1.png" alt="">
-        </div>
-        <div class="d-flex justify-content-center align-items-center flex-column mx-4">
-          <h1 class="s_orange">20 K</h1>
-          <h5>Client Satyisfaction</h5>
-        </div>
-        <div>
-          <img src="../assets/counter-shap1.png" alt="">
-        </div>
-        <div class="d-flex justify-content-center align-items-center flex-column mx-4">
-          <h1 class="s_orange">30 +</h1>
-          <h5>Award Winning</h5>
-        </div>
-      </div>
+  <div class="statistiche container d-flex justify-content-center align-items-center my-5">
+    <div class="d-flex justify-content-center align-items-center flex-column mx-4">
+      <h1 class="s_orange">{{ years }} +</h1>
+      <h5>Years of Experienced</h5>
+    </div>
+    <div>
+      <img src="../assets/counter-shap1.png" alt="">
+    </div>
+    <div class="d-flex justify-content-center align-items-center flex-column mx-4">
+      <h1 class="s_orange">{{ count }} +</h1>
+      <h5>Everyday Online Delivery</h5>
+    </div>
+    <div>
+      <img src="../assets/counter-shap1.png" alt="">
+    </div>
+    <div class="d-flex justify-content-center align-items-center flex-column mx-4">
+      <h1 class="s_orange">{{ client }} K</h1>
+      <h5>Client Satyisfaction</h5>
+    </div>
+    <div>
+      <img src="../assets/counter-shap1.png" alt="">
+    </div>
+    <div class="d-flex justify-content-center align-items-center flex-column mx-4">
+      <h1 class="s_orange">{{ award }} +</h1>
+      <h5>Award Winning</h5>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
@@ -44,6 +89,7 @@ export default {
 
 .statistiche {
   padding: 40px 0;
+
   h1 {
     font-size: 50px;
     font-weight: bold;
