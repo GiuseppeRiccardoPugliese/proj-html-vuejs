8e2c2df30c48215348828898ef8041401c3bd59d
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-01-18 17:36:57 +0100

    2nd Slider fix

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -36,7 +36,11 @@ export default {
     OrganicFoodDiscount,
     TopHomeFood
   },
-
+  data() {
+    return {
+      isHovered: false,
+    }
+  },
   setup() {
     return {
       modules: [Autoplay, Pagination, Navigation]
@@ -125,10 +129,26 @@ export default {
       </div>
       <!-- Carousel -->
       <div class="col-12 my-4">
-        <swiper :slidesPerView="4" :spaceBetween="30" :loop="true" :autoplay="{
-          delay: 2500,
-          disableOnInteraction: false
-        }" :navigation="true" :modules="modules" class="mySwiper">
+        <swiper @mouseover="isHovered = true" @mouseleave="isHovered = false" :navigation="isHovered ? true : false"
+          :style="{
+            '--swiper-navigation-color': '#000'
+          }" :loop="true" :autoplay="{
+  delay: 2500,
+  disableOnInteraction: false
+}" :modules="modules" :breakpoints="{
+  '300': {
+    slidesPerView: 1,
+    spaceBetween: 0
+  },
+  '800': {
+    slidesPerView: 3,
+    spaceBetween: 10
+  },
+  '1200': {
+    slidesPerView: 4,
+    spaceBetween: 0
+  }
+}" class="mySwiper">
           <swiper-slide class="d-flex justify-content-center align-items-center">
             <img src="../../public/fe1.jpg" alt="" />
           </swiper-slide>
